2ce8ac0ac0ea3fd89930a8dd390315a47ab04a4f
Ton Nguyen <tonnguyen@axon.com>
2022-04-27 08:17:34 +0700

    Update logic

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function () {
     const [selectedDeviceId, setSelectedDeviceId] = useState("");
     const [text, setText] = useState("");
     const [error, setError] = useState("");
+    const [scanning, setScanning] =useState(false);
 
     const pdf417Reader = new BrowserPDF417Reader();
 
@@ -40,46 +41,60 @@ export default function () {
     }
 
 
+    function decodeOnce() {
+        pdf417Reader.decodeOnceFromVideoDevice(selectedDeviceId, 'video').then((result) => {
+            console.log(result)
+            alert("Text result: " + result.text);
+            resetClick();
+        }).catch((err) => {
+            console.error(err)
+        })
+    }
 
-    useEffect(() => {
-            if(selectedDeviceId)
-            {
-                pdf417Reader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
-                    if (result) {
-                        console.log(result)
-                        alert("Text result: " + result.text);
-                        resetClick();
-                    }
-                    if (err && !(err instanceof NotFoundException)) {
-                        console.error(err)
-                    }
-                })
-                // pdf417Reader.scanOneResult('video', false, true, false)
-                console.log(`Started decode from camera with id ${ selectedDeviceId }`);
+    function decodeContinuously () {
+        pdf417Reader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+            if (result) {
+                console.log(result)
+
             }
-        },
-        [selectedDeviceId]
-    );
+            if (err && !(err instanceof NotFoundException)) {
+                console.error(err)
+            }
+        })
+    }
 
 
-    return (<Space direction={ "vertical" } size={ 30 }>
+    const startScanning = () => {
+        if(selectedDeviceId)
+        {
+            console.log(`Started decode from camera with id ${ selectedDeviceId }`);
+            decodeOnce()
+        }
+    }
+
+    return (<Space direction={ "vertical" } size={ 20 } style={{display: 'flex', justifyContent: 'center', alignItems:' center', marginTop:30}}>
         <h1>PDF417 Scanner demo (zxing)</h1>
         <div>Use <a href="https://barcode.tec-it.com/en/PDF417" target={ "_blank" }>THIS LINK</a> to generate barcode
             for testing
         </div>
 
+       <div>
+           <div>Select camera:</div>
+           <Select value={selectedDeviceId} style={{width: 240}} onChange={value => setSelectedDeviceId(value)}>
+               {
+                   videoInputDevices.map(element => (
+                       <Option key={element.deviceId} value={element.deviceId}>{element.label}</Option>
+                   ))
+               }
+           </Select>
+       </div>
+        <Space size={20}>
 
-        <Space>
-            <Select>
-                {}
-            </Select>
-            <Button type={ "primary" } onClick={ () => {
-                setSelectedDeviceId(videoInputDevices?.[0]?.deviceId);
-            } }> Scan barcode</Button>
-            {/*<Button onClick={() => resetClick() }> Reset </Button>*/ }
+            <Button type={ "primary" } onClick={ startScanning } disabled={!selectedDeviceId}> Scan barcode</Button>
+            <Button onClick={() => resetClick() }> Reset </Button>
         </Space>
         { error && <div>Error: <br />{ error }</div> }
-        <video id="video" height={400} />
+        { selectedDeviceId && <video id="video" height={ 400 } /> }
     </Space>)
 
 }
